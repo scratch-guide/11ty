5290f3dcdd85ccbdac974251238d2fe4ac96364c
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-04-18 16:27:43 +0200

    Create basic internationalization config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,15 @@
-module.exports = function(eleventyConfig) {
+module.exports = function(config) {
+	config.addPassthroughCopy("./src/css/");
+	config.addWatchTarget("./src/css/");
+
+	config.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+	config.addFilter('localizedPermalink', /** @param {String} input */ function(input) {
+		const split = input.split('/');
+		split.unshift(split.pop() || split.pop());
+		return split.join('/');
+	})
+
 	return {
 		dir: {
 			input: 'src',
